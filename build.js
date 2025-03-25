082b3354822ae9e271deeb6c20525175a4910a7f
Pablo Fernández Franco <pableudo@gmail.com>
2025-03-25 19:32:32 +0000

    wip

--- build.js
+++ build.js
@@ -1,5 +1,39 @@
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+import ThemesLoader from 'sd-themes-loader';
+
+register(StyleDictionary, {
+    withSDBuiltins: false,
+});
+
+const loader = ThemesLoader(StyleDictionary);
+
 async function run() {
- console.log('hola')
+    const themes = await loader.load('/tokens');
+
+    const globalTheme = themes.getThemeByName('global');
+
+    const config = {
+        platforms: {
+            web: {
+                files: [
+                    {
+                        destination: 'app/build/global/variables.css',
+                        format: 'css/variables',
+                    }
+                ],
+
+                transforms: [
+                    'name/kebab',
+                    'ts/resolveMath',
+                    'size/pxToRem'
+                ]
+            }
+        }
+    };
+
+    globalTheme.addConfig(config).build();
+
 }
 
 run();
